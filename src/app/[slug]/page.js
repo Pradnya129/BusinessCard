520d6883d89b02626fdf151c5558f29f3e7dd18e
admin <admin@gmail.com>
2025-09-22 17:05:57 +0530

    notfound

--- src/app/[slug]/page.js
+++ src/app/[slug]/page.js
@@ -1,4 +1,6 @@
 import LandingPage from "../LandingPage/LandingPage.js";
+import Link from "next/link";
+import { notFound } from "next/navigation";
 
 export default async function Home({ params }) {
   const { slug } = params; // get slug from URL
@@ -8,41 +10,15 @@ export default async function Home({ params }) {
     cache: 'no-store',
   });
 
-  // If backend returns 404 or error, show not found
   if (!res.ok) {
-    return (
-      <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-gray-700">
-        <h1 className="text-6xl font-bold mb-4">404</h1>
-        <p className="text-2xl mb-6">Landing Page Not Found</p>
-        <p className="mb-6">We couldn’t find the landing page for "{slug}"</p>
-        <a 
-          href="/" 
-          className="px-6 py-3 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
-        >
-          Go Home
-        </a>
-      </div>
-    );
+    // Use Next.js 404 handling
+    notFound();
   }
 
   const data = await res.json();
 
   if (!data.admin) {
-    return (
-     < div className="container ">
-         <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-gray-700">
-        <h1 className="text-6xl font-bold mb-4">404</h1>
-        <p className="text-2xl mb-6">Landing Page Not Found</p>
-        <p className="mb-6">We couldn’t find the landing page for "{slug}"</p>
-        <a 
-          href="/" 
-          className="px-6 py-3 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
-        >
-          Go Home
-        </a>
-      </div>
-     </div>
-    );
+    notFound();
   }
 
   // Pass admin to LandingPage
